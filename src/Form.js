4faedb294169daf980a921ca89e74b0bce185d40
realSaddy <jrp30074@gmail.com>
2020-03-05 13:35:02 -0600

    Testing state children w/ textfield, first success. Adding room

--- src/Form.js
+++ src/Form.js
@@ -1,32 +1,57 @@
 import React from 'react';
-import {TextField} from '@material-ui/core';
+import {TextField, MenuItem} from '@material-ui/core';
 
 
 class NameForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {first_name:''};
-
+        this.state = {first_name:'', room_number:''};
+        this.rooms = {
+            "": [
+                "test"
+            ],
+            "100": [
+                "George",
+                "Greg",
+                "Dan"
+            ]
+        };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        this.setState({first_name: event.target.value});
+        this.setState({...this.state, [event.target.name]: event.target.value});
     }
 
     render() {
         return (
             <form>
-                <div>
+                <div style={{paddingTop:15}}>
                     <TextField
+                        name="first_name"
                     id="select-first-name"
                     value={this.state.first_name}
                     onChange={this.handleChange}
                     label="First Name"
-                    helperText="Please put your first name"
                     variant="outlined">
-                        {this.state.first_name}
                     </TextField>
+                </div>
+                <div style={{paddingTop:15}}>
+                    <TextField
+                        name="room_number"
+                        id="room-number"
+                        select
+                        children={this.state.room_number}
+                        value={this.state.room_number}
+                        onChange={this.handleChange}
+                        label="Room Number"
+                        variant="outlined">
+                    </TextField>
+                    {this.rooms[this.state.room_number].map(option=>(
+                        <MenuItem key={option} value={option}>
+                            {option}
+                        </MenuItem>
+                    ))}
                 </div>
             </form>
         );
