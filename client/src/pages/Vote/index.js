dc7a0a1d0261e908bff2be1423162ab9d0847261
realSaddy <jrp30074@gmail.com>
2020-03-07 12:40:05 -0600

    Corrected favicons

--- client/src/pages/Vote/index.js
+++ client/src/pages/Vote/index.js
@@ -25,8 +25,6 @@ class  Vote extends React.Component {
             <div className="App">
                 <Header handler={this.loginHandler}/>
                 { this.state.enableForm ? < LoginForm handler={this.loginHandler} />: ""}
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 <NameForm/>
             </div>
         );
