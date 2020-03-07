6a8f3e5112d7f2f49643cfb8ba2f75c5a262628b
realSaddy <jrp30074@gmail.com>
2020-03-07 11:22:12 -0600

    Made header a class, added onclick for login button

--- client/src/pages/Vote/Header.js
+++ client/src/pages/Vote/Header.js
@@ -5,17 +5,28 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 
-function Header() {
-    return (
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.showLoginForm = this.showLoginForm.bind(this);
+    }
+
+    showLoginForm() {
+        console.log("hi")
+    }
+    render(){
+        return (
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6">
                         JSA Voting
                     </Typography>
-                    <Button style={{marginLeft: 'auto'}} color="inherit">Login</Button>
+                    <Button onClick={this.showLoginForm} style={{marginLeft: 'auto'}} color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
-    );
+        );
+    }
 }
 
 export default Header;
