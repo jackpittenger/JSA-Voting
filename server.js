ef62d76b25de1f138973a9602eee76df132fd898
realSaddy <jrp30074@gmail.com>
2020-03-10 16:23:58 -0500

    Deployment on node

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
 const port = 5000;
 const { login } = require("./mongo");
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get("/testing", (req, res)=>{
     res.send("Test");
@@ -15,6 +17,10 @@ app.post("/api/login", (req, res)=>{
     login(req.body.token, req.body.pin)
         .then(ret=>console.log(ret));
     return res.send("Hi!");
+});
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname+"/client/build/index.html"))
 });
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
