3e796c22b087202d890f9fff448e783eab6ba89a
realSaddy <jrp30074@gmail.com>
2020-05-21 16:23:17 -0500

    Force room code

--- mongo/index.js
+++ mongo/index.js
@@ -198,24 +198,33 @@ module.exports.authenticateCode = (req, res) => {
         return res
           .status(409)
           .json({ error: "User already registered for this room!" });
-      Voter.create({
-        firstName: req.body.first_name,
-        code: req.body.code,
-        lastName: req.body.last_name,
-        school: req.body.school,
-      }).then((usr) => {
-        console.log(usr + " created!");
-        let payload = {
-          firstName: usr.firstName,
-          code: usr.code,
-          lastName: usr.lastName,
-          school: usr.school,
-        };
-        let jwtToken = jwt.sign(payload, process.env.SECRET, {
-          expiresIn: "1h",
+      Room.findOne({ accessCode: req.body.code })
+        .then((room) => room)
+        .then((room) => {
+          if (!room) return res.status(409).json({ error: "Incorrect code!" });
+          Voter.create({
+            firstName: req.body.first_name,
+            code: req.body.code,
+            lastName: req.body.last_name,
+            school: req.body.school,
+          }).then((usr) => {
+            room.users.push(usr._id);
+            room.save();
+            console.log(usr + " created!");
+            let payload = {
+              firstName: usr.firstName,
+              code: usr.code,
+              lastName: usr.lastName,
+              school: usr.school,
+            };
+            let jwtToken = jwt.sign(payload, process.env.SECRET, {
+              expiresIn: "1h",
+            });
+            return res
+              .status(201)
+              .send({ token: jwtToken, message: "Success" });
+          });
         });
-        return res.status(201).send({ token: jwtToken, message: "Success" });
-      });
     })
     .catch(() => {
       return res.status(500).json({ error: "Error fetching voter" });
