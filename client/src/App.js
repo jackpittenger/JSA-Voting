4007ffdcf1d8741abcd09414704c4cb126725dad
realSaddy <jrp30074@gmail.com>
2020-07-06 17:02:54 -0500

    Fix importation error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Router from "react-router-dom/Router";
+import { Router } from "react-router-dom";
 
 import history from "./services/history";
 import Routes from "./routes";
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Switch from "react-router-dom/Switch";
-import Route from "react-router-dom/Route";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Dashboard from "../pages/Dashboard";
 import Vote from "../pages/Vote";
