fbad7092de0e4abd84b6b42b93b17688123efc4a
realSaddy <jrp30074@gmail.com>
2020-07-26 15:01:40 -0500

    NODE_ENV switch for socket

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import openSocket from "socket.io-client";
 
 function openSoc(token) {
-  let socket = openSocket("http://localhost:8000");
+  let socket = openSocket(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000"
+      : "https://jsavote.com:8000"
+  );
   socket.emit("token", token);
   socket.on("unauthorized", () => socket.disconnect());
   return socket;
