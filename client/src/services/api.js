f3f2160cb8c1e77fd8d0bca01f9b63b18bc7fca7
realSaddy <jrp30074@gmail.com>
2020-07-26 15:25:41 -0500

    Port 8443 for prod socket

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ function openSoc(token) {
   let socket = openSocket(
     process.env.NODE_ENV === "development"
       ? "http://localhost:8000"
-      : "https://jsavote.com:8000"
+      : "https://jsavote.com:8443"
   );
   socket.emit("token", token);
   socket.on("unauthorized", () => socket.disconnect());
