557d696c9181f1e1bf8c9569b9e26a4f4aedd5f3
realSaddy <jrp30074@gmail.com>
2020-08-22 10:53:07 -0500

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,10 @@ import { Router } from "react-router-dom";
 import history from "./services/history";
 import Routes from "./routes";
 
-function App() {
+export default function App() {
   return (
     <Router history={history}>
       <Routes />
     </Router>
   );
 }
-
-export default App;
