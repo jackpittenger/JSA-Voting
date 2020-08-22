c313b60e0643b5e13afba188312af67b39cfb3da
realSaddy <jrp30074@gmail.com>
2020-08-22 11:45:27 -0500

    Functionization of dashboard

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState } from "react";
 import withAuth from "../../services/withAuth";
 import Header from "../../components/Header";
 import AuthService from "../../services/AuthService";
@@ -7,34 +6,19 @@ import Mod from "./Mod";
 import Admin from "./Admin";
 import Dev from "./Dev";
 
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      permissions: "",
-    };
-    this.Auth = new AuthService();
-  }
+function Dashboard() {
+  const auth = new AuthService();
+  const data = auth.getProfile();
+  const permissions = useState(data.permissions)[0];
 
-  componentDidMount() {
-    let data = this.Auth.getProfile();
-    this.setState({
-      name: data.token,
-      permissions: data.permissions,
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header auth={this.Auth} />
-        {this.state.permissions.indexOf("Mod") !== -1 ? <Mod /> : null}
-        {this.state.permissions.indexOf("Admin") !== -1 ? <Admin /> : null}
-        {this.state.permissions.indexOf("Dev") !== -1 ? <Dev /> : null}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header auth={auth} />
+      {permissions.indexOf("Mod") !== -1 ? <Mod /> : null}
+      {permissions.indexOf("Admin") !== -1 ? <Admin /> : null}
+      {permissions.indexOf("Dev") !== -1 ? <Dev /> : null}
+    </div>
+  );
 }
 
 export default withAuth(Dashboard);
