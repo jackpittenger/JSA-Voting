d27aed17694d197c60afbd94a5b3f2a7e9f3874f
realSaddy <jrp30074@gmail.com>
2020-09-25 20:20:46 -0500

    Stylistic changes

--- client/src/pages/Rooms/index.js
+++ client/src/pages/Rooms/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 
 import Paper from "@material-ui/core/Paper";
@@ -8,10 +9,27 @@ import Typography from "@material-ui/core/Typography";
 import Header from "../../components/Header";
 import AuthService from "../../services/AuthService";
 
+const useStyles = makeStyles(() => ({
+  grid: {
+    margin: 10,
+    justifyContent: "center",
+  },
+  gridItem: {
+    flexGrow: "unset",
+  },
+  item: {
+    width: "30em",
+    height: "10em",
+    padding: "1.2em",
+  },
+}));
+
 export default function Room() {
   const Auth = new AuthService();
   const [page, setPage] = useState(0);
   const [rooms, setRooms] = useState([]);
+  const classes = useStyles();
+
   useEffect(() => {
     axios
       .get("/api/page/" + page)
@@ -22,29 +40,27 @@ export default function Room() {
   return (
     <div>
       <Header auth={Auth} />
-      <div>
-        <Grid container spacing={3}>
-          {rooms === []
-            ? null
-            : rooms.map((v, i) => {
-                return (
-                  <Grid item xs key={i}>
-                    <Paper>
-                      <Typography gutterBottom variant="subtitle1">
-                        {v.id}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        ID: {v._id}
-                      </Typography>
-                      <Typography variant="subtitle1">
-                        {v.yea}-{v.abs}-{v.nay}
-                      </Typography>
-                    </Paper>
-                  </Grid>
-                );
-              })}
-        </Grid>
-      </div>
+      <Grid container spacing={3} className={classes.grid}>
+        {rooms === []
+          ? null
+          : rooms.map((v, i) => {
+              return (
+                <Grid item xs key={i} className={classes.gridItem}>
+                  <Paper className={classes.item}>
+                    <Typography gutterBottom variant="subtitle1">
+                      {v.id}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      ID: {v._id}
+                    </Typography>
+                    <Typography variant="subtitle1">
+                      {v.yea}-{v.abs}-{v.nay}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              );
+            })}
+      </Grid>
     </div>
   );
 }
