da15601e9978e0b54ce5ee111590585a736e18e1
realSaddy <jrp30074@gmail.com>
2020-10-11 09:06:58 -0500

    Backend transfer to number-based permissions

--- mongo/models/User.js
+++ mongo/models/User.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   token: { type: String, unique: true, required: true },
   pin: { type: String, required: true },
-  permissions: { type: [String], required: true },
+  permission: { type: Number, required: true },
+  /*
+   * 0: No access
+   * 1: Mod
+   * 2: Admin
+   * 3: Dev
+   * */
   room: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Room",
