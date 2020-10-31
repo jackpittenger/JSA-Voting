e69fb71f4fee84c4f06dd8a8b09c1bf751d27172
realSaddy <jrp30074@gmail.com>
2020-10-31 17:53:04 -0500

    Cleanup server.js

--- server.js
+++ server.js
@@ -9,27 +9,27 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 const {
-  login,
-  createUser,
   authenticateCode,
+  addSpeaker,
+  clearRooms,
+  concludeRoom,
   createRoom,
-  getRoom,
+  createUser,
   deleteRoom,
+  deleteUser,
+  getRoom,
+  getSpeakers,
+  getTotalPages,
+  login,
+  page,
+  removeSpeaker,
+  roomsOpen,
+  roomsToggle,
+  speakerVote,
   submitForm,
   toggleOpen,
   toggleVoting,
-  deleteUser,
-  concludeRoom,
-  page,
-  getTotalPages,
   updateRoomByline,
-  addSpeaker,
-  removeSpeaker,
-  speakerVote,
-  getSpeakers,
-  roomsToggle,
-  roomsOpen,
-  clearRooms,
 } = require("./mongo");
 const privateKey = fs.readFileSync("server.key", "utf8");
 const certificate = fs.readFileSync("server.crt", "utf8");
@@ -39,27 +39,30 @@ app.use(cookieParser());
 require("./middleware/socket").setup(io);
 app.use(bodyParser.json());
 
-app.post("/api/login", login);
-app.post("/api/create_user", createUser);
+app.get("/api/get_speakers", getSpeakers);
+app.get("/api/page/:page", page);
+app.get("/api/rooms_open", roomsOpen);
+app.get("/api/total_pages", getTotalPages);
+
+app.post("/api/add_speaker", addSpeaker);
 app.post("/api/auth_code", authenticateCode);
 app.post("/api/create_room", createRoom);
+app.post("/api/create_user", createUser);
 app.post("/api/get_room", getRoom);
+app.post("/api/login", login);
+app.post("/api/speaker_vote", speakerVote);
 app.post("/api/submit_form", submitForm);
-app.delete("/api/room", deleteRoom);
 app.post("/api/toggle_open", toggleOpen);
 app.post("/api/toggle_voting", toggleVoting);
-app.delete("/api/delete_user", deleteUser);
+
 app.patch("/api/conclude_room", concludeRoom);
-app.get("/api/page/:page", page);
-app.get("/api/total_pages", getTotalPages);
 app.patch("/api/room_byline", updateRoomByline);
-app.post("/api/add_speaker", addSpeaker);
-app.delete("/api/remove_speaker", removeSpeaker);
-app.post("/api/speaker_vote", speakerVote);
-app.get("/api/get_speakers", getSpeakers);
 app.patch("/api/rooms_toggle", roomsToggle);
-app.get("/api/rooms_open", roomsOpen);
+
 app.delete("/api/clear_rooms", clearRooms);
+app.delete("/api/delete_user", deleteUser);
+app.delete("/api/remove_speaker", removeSpeaker);
+app.delete("/api/room", deleteRoom);
 
 if (process.env.DEPLOY === "true") {
   app.use(express.static(path.join(__dirname, "./client/build")));
