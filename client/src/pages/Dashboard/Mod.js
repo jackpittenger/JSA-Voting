0834157766701638faef305b47f5525a91833126
realSaddy <jrp30074@gmail.com>
2021-01-03 20:35:02 -0600

    Removing mongo

--- client/src/pages/Dashboard/Mod.js
+++ client/src/pages/Dashboard/Mod.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import Room from "./Room";
-
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
@@ -10,7 +8,6 @@ import ErrorPopup from "../../components/ErrorPopup";
 
 function Mod(props) {
   const [roomName, setRoomName] = useState("");
-  const [room, setRoom] = useState(null);
   const [roomNameValid, setRoomNameValid] = useState(false);
   const [buttonValid, setButtonValid] = useState(false);
 
@@ -24,12 +21,6 @@ function Mod(props) {
     setButtonValid(roomName.length !== 0 && roomNameValid);
   }, [roomName, roomNameValid]);
 
-  useEffect(() => {
-    props.auth.fetch("/api/get_room", { method: "POST" }, (res, status) => {
-      if (status < 300) setRoom(res);
-    });
-  }, [props.auth]);
-
   function createRoom() {
     props.auth.fetch(
       "/api/create_room",
@@ -39,7 +30,6 @@ function Mod(props) {
           props.createError(status, res.error);
         } else {
           setRoomName("");
-          setRoom(res);
         }
       }
     );
@@ -48,31 +38,23 @@ function Mod(props) {
   return (
     <Grid container direction="column" justify="center" alignItems="stretch">
       <Paper>
-        {room != null ? (
-          <Room disable={() => setRoom(null)} auth={props.auth} room={room} />
-        ) : (
-          <div>
-            Mod
-            <br />
-            <TextField
-              name="room_name"
-              id="room_name"
-              label="Room name"
-              type="text"
-              onChange={(e) => setRoomName(e.target.value)}
-              value={roomName}
-              error={!roomNameValid}
-              helperText="Between 1-32 characters"
-            />
-            <Button
-              disabled={!buttonValid}
-              onClick={createRoom}
-              color="primary"
-            >
-              Create a new room
-            </Button>
-          </div>
-        )}
+        <div>
+          Mod
+          <br />
+          <TextField
+            name="room_name"
+            id="room_name"
+            label="Room name"
+            type="text"
+            onChange={(e) => setRoomName(e.target.value)}
+            value={roomName}
+            error={!roomNameValid}
+            helperText="Between 1-32 characters"
+          />
+          <Button disabled={!buttonValid} onClick={createRoom} color="primary">
+            Create a new room
+          </Button>
+        </div>
       </Paper>
     </Grid>
   );
